de12e36df92105be02b7782aac1e91ebe818bae4
mystagogy <hi7747@naver.com>
2023-02-16 01:05:21 +0900

    #17 fix : LoanProduct와 Like에 Exception 추가에 따른 핸들러 수정

--- src/main/java/com/miniproject/backend/global/exception/CustomExceptionHandler.java
+++ src/main/java/com/miniproject/backend/global/exception/CustomExceptionHandler.java
@@ -2,6 +2,8 @@ package com.miniproject.backend.global.exception;
 
 import com.miniproject.backend.global.dto.ErrorDTO;
 import com.miniproject.backend.global.exception.base.CustomException;
+import com.miniproject.backend.like.exception.LikeException;
+import com.miniproject.backend.loanproduct.exception.ProductException;
 import com.miniproject.backend.user.exception.UserException;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
@@ -15,6 +17,22 @@ public class CustomExceptionHandler implements ErrorController {
     //custom exception 처리 핸들러
     @ExceptionHandler(value = UserException.class)
     public ErrorDTO handleUserException(CustomException ce){
+        return ErrorDTO.builder()
+                .errorCode(ce.getExceptionType().getErrorCode())
+                .errorMessage(ce.getExceptionType().getMessage())
+                .build();
+    }
+
+    @ExceptionHandler(value = ProductException.class)
+    public ErrorDTO handleProductException(CustomException ce){
+        return ErrorDTO.builder()
+                .errorCode(ce.getExceptionType().getErrorCode())
+                .errorMessage(ce.getExceptionType().getMessage())
+                .build();
+    }
+
+    @ExceptionHandler(value = LikeException.class)
+    public ErrorDTO handleLikeException(CustomException ce){
         return ErrorDTO.builder()
                 .errorCode(ce.getExceptionType().getErrorCode())
                 .errorMessage(ce.getExceptionType().getMessage())
