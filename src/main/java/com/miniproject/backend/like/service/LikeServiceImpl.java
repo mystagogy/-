9a265e505bad83d6f756be889acf5df0d8a5d290
mystagogy <hi7747@naver.com>
2023-02-16 01:04:44 +0900

    #17 feat : 이미 등록된 상품에 대한 Exception 추가

--- src/main/java/com/miniproject/backend/like/service/LikeServiceImpl.java
+++ src/main/java/com/miniproject/backend/like/service/LikeServiceImpl.java
@@ -2,6 +2,8 @@ package com.miniproject.backend.like.service;
 
 import com.miniproject.backend.like.domain.Like;
 import com.miniproject.backend.like.dto.LikeResponseDto;
+import com.miniproject.backend.like.exception.LikeException;
+import com.miniproject.backend.like.exception.LikeExceptionType;
 import com.miniproject.backend.like.repository.LikeRepository;
 import com.miniproject.backend.loanproduct.domain.LoanProduct;
 import com.miniproject.backend.loanproduct.service.ProductService;
@@ -20,26 +22,27 @@ public class LikeServiceImpl implements LikeService{
     private final ProductService productService;
 
     public LikeResponseDto addLike(String productId, String userEmail) {
-        //TODO : Null 처리
-
 
         User user = userService.findUserByUserId(userEmail);
         LoanProduct loanProduct = productService.findProductByProductId(productId);
-        Like like = Like.builder()
-                .loanProduct(loanProduct)
-                .user(user)
-                .build();
-        likeRepository.save(like);
+        if(!likeRepository.existsByUserAndLoanProduct(user,loanProduct)){
+            Like like = Like.builder()
+                    .loanProduct(loanProduct)
+                    .user(user)
+                    .build();
+            likeRepository.save(like);
 
-        LikeResponseDto likeResponseDto = LikeResponseDto.builder()
-                .likeId(like.getId())
-                .bankImgPath(like.getLoanProduct().getBank().getImgPath())
-                .bankName(like.getLoanProduct().getBank().getBankNm())
-                .productName(like.getLoanProduct().getProductNm())
-                .loanRateList(like.getLoanProduct().getLoanRates())
-                .loanLimit(like.getLoanProduct().getLoanLimit())
-                .build();
-
-        return likeResponseDto;
+            LikeResponseDto likeResponseDto = LikeResponseDto.builder()
+                    .likeId(like.getId())
+                    .bankImgPath(like.getLoanProduct().getBank().getImgPath())
+                    .bankName(like.getLoanProduct().getBank().getBankNm())
+                    .productName(like.getLoanProduct().getProductNm())
+                    .loanRateList(like.getLoanProduct().getLoanRates())
+                    .loanLimit(like.getLoanProduct().getLoanLimit())
+                    .build();
+            return likeResponseDto;
+        }else{
+            throw new LikeException(LikeExceptionType.EXIST_LIKE);
+        }
     }
 }
