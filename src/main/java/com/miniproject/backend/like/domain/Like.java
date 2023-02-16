5e53067df22d41f1e8c4efd24bb186d122df2f8c
mystagogy <hi7747@naver.com>
2023-02-16 22:52:19 +0900

    conf : null Exception 처리을 위한 타입 변환
    primitive type to reference type

--- src/main/java/com/miniproject/backend/like/domain/Like.java
+++ src/main/java/com/miniproject/backend/like/domain/Like.java
@@ -19,7 +19,7 @@ public class Like {
 
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "likes_id")
-    private long id;
+    private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
