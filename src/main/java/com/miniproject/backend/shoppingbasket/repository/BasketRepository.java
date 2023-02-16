0a883ceea36bf51bd6285e0b7824132969e18e56
hamings <wjdgpal9630@gmail.com>
2023-02-16 14:22:39 +0900

    #20 feat: repository 장바구니 등록여부 확인, 삭제 함수 추가

--- src/main/java/com/miniproject/backend/shoppingbasket/repository/BasketRepository.java
+++ src/main/java/com/miniproject/backend/shoppingbasket/repository/BasketRepository.java
@@ -1,9 +1,19 @@
 package com.miniproject.backend.shoppingbasket.repository;
 
+import com.miniproject.backend.loanproduct.domain.LoanProduct;
 import com.miniproject.backend.shoppingbasket.domain.Basket;
+import com.miniproject.backend.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface BasketRepository extends JpaRepository<Basket,Long> {
+
+    boolean existsByUserAndLoanProduct(User user,LoanProduct loanProduct);
+
+
+    int deleteByUserAndLoanProduct(User user,LoanProduct loanProduct);
+
+
 }
