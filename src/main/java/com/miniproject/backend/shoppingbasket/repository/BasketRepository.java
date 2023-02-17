5d11fc85d0bb923072c15a600252542839f5f4be
hamings <wjdgpal9630@gmail.com>
2023-02-17 13:46:09 +0900

    #34 feat: 유저로 찾는 함수, 유저와 basketId로 찾는 함수 추가

--- src/main/java/com/miniproject/backend/shoppingbasket/repository/BasketRepository.java
+++ src/main/java/com/miniproject/backend/shoppingbasket/repository/BasketRepository.java
@@ -6,14 +6,18 @@ import com.miniproject.backend.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 
 @Repository
 public interface BasketRepository extends JpaRepository<Basket,Long> {
 
     boolean existsByUserAndLoanProduct(User user,LoanProduct loanProduct);
 
+    List<Basket> findByUser(User user);
 
-    int deleteByUserAndLoanProduct(User user,LoanProduct loanProduct);
+    Optional<Basket> findByIdAndUser(Long basketId, User user);
 
 
 }
