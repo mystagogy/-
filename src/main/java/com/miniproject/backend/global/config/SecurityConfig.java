62a4aa279ec51cc268a8f5217d139a1a1384f569
HongGuma <hongsu9849@gmail.com>
2023-02-20 01:30:41 +0900

    config : jwt filter 실행시키는 설정 추가

--- src/main/java/com/miniproject/backend/global/config/SecurityConfig.java
+++ src/main/java/com/miniproject/backend/global/config/SecurityConfig.java
@@ -1,17 +1,27 @@
 package com.miniproject.backend.global.config;
 
+import com.miniproject.backend.global.jwt.JwtAuthorizationFilter;
+import com.miniproject.backend.global.jwt.auth.AuthTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final AuthTokenProvider authTokenProvider;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
@@ -28,6 +38,9 @@ public class SecurityConfig {
                 .permitAll()
                 .anyRequest().authenticated();
 
+        http
+                .apply(new customConfig());
+
         return http.build();
     }
 
@@ -36,4 +49,12 @@ public class SecurityConfig {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
+    public class customConfig extends AbstractHttpConfigurer<customConfig, HttpSecurity> {
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
+            http
+                    .addFilterAfter(new JwtAuthorizationFilter(authenticationManager, authTokenProvider), CorsFilter.class);
+        }
+    }
 }
