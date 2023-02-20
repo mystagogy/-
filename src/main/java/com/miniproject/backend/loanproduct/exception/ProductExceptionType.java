c91eed54a0c60979b2ab61a484f6f195545b774e
mystagogy <hi7747@naver.com>
2023-02-20 13:53:44 +0900

    #38 feat : 검색결과가 없는 경우 Exception 추가

--- src/main/java/com/miniproject/backend/loanproduct/exception/ProductExceptionType.java
+++ src/main/java/com/miniproject/backend/loanproduct/exception/ProductExceptionType.java
@@ -3,7 +3,8 @@ package com.miniproject.backend.loanproduct.exception;
 import com.miniproject.backend.global.exception.base.CustomExceptionType;
 
 public enum ProductExceptionType implements CustomExceptionType {
-    PRODUCT_NOT_EXIST(-201, "상품정보가 존재하지 않습니다.");
+    PRODUCT_NOT_EXIST(-201, "상품정보가 존재하지 않습니다."),
+    SEARCH_NOT_EXIST(-202, "검색어에 일치하는 상품이 존재하지 않습니다.");
 
     private int errorCode;
     private String errorMsg;
