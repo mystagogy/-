d2adf71b19926c0df83eea6ce601b68f48071acd
mystagogy <hi7747@naver.com>
2023-02-20 19:25:14 +0900

    #51 refactor : 사용자 이메일 토큰 추출로 변경

--- src/main/java/com/miniproject/backend/like/service/LikeService.java
+++ src/main/java/com/miniproject/backend/like/service/LikeService.java
@@ -1,5 +1,6 @@
 package com.miniproject.backend.like.service;
 
+import com.miniproject.backend.global.jwt.CustomUserDetails;
 import com.miniproject.backend.like.dto.LikeRequestDto;
 import com.miniproject.backend.like.dto.LikeResponseDto;
 import com.miniproject.backend.user.domain.User;
@@ -7,9 +8,9 @@ import com.miniproject.backend.user.domain.User;
 import java.util.List;
 
 public interface LikeService {
-    LikeResponseDto addLike(LikeRequestDto likeRequestDto);
+    LikeResponseDto addLike(String email, String productId);
 
-    List<LikeResponseDto> selectAllLike(String userEmail);
+    List<LikeResponseDto> selectAllLike(String email);
 
     List<LikeResponseDto> deleteLike(String userEmail, long Id);
 
