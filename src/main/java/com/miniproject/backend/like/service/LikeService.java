ee16c342f2fa9cce21867570a496dbf92e2853d3
mystagogy <hi7747@naver.com>
2023-02-20 23:13:40 +0900

    #53 refactor : 변경된 DTO 적용

--- src/main/java/com/miniproject/backend/like/service/LikeService.java
+++ src/main/java/com/miniproject/backend/like/service/LikeService.java
@@ -1,17 +1,16 @@
 package com.miniproject.backend.like.service;
 
-import com.miniproject.backend.global.jwt.CustomUserDetails;
-import com.miniproject.backend.like.dto.LikeRequestDto;
-import com.miniproject.backend.like.dto.LikeResponseDto;
-import com.miniproject.backend.user.domain.User;
+
+import com.miniproject.backend.like.dto.LikeDto;
+
 
 import java.util.List;
 
 public interface LikeService {
-    LikeResponseDto addLike(String email, String productId);
+    LikeDto.Response addLike(String email, String productId);
 
-    List<LikeResponseDto> selectAllLike(String email);
+    List<LikeDto.Response> selectAllLike(String email);
 
-    List<LikeResponseDto> deleteLike(String userEmail, long Id);
+    List<LikeDto.Response> deleteLike(String userEmail, long Id);
 
 }
