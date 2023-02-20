713a4cb4dda6c9efa5ce3c34539a8eeef50f1854
hamings <wjdgpal9630@gmail.com>
2023-02-20 13:24:46 +0900

    #40 feat: service 구매리스트 출력, 구매삭제 기능 추가

--- src/main/java/com/miniproject/backend/shoppingbasket/service/OrderService.java
+++ src/main/java/com/miniproject/backend/shoppingbasket/service/OrderService.java
@@ -3,10 +3,16 @@ package com.miniproject.backend.shoppingbasket.service;
 import com.miniproject.backend.shoppingbasket.domain.Basket;
 import com.miniproject.backend.shoppingbasket.dto.BasketResponseDTO;
 
+import java.util.List;
+
 public interface OrderService {
 
     BasketResponseDTO buyCart(String email, String productId);
 
     Basket findBasketByUserAndLoanProduct(String email, String productId);
+
+    List<BasketResponseDTO> selectBuyList(String email);
+
+    String deleteBuy(String email,Long basketId);
 
 }
