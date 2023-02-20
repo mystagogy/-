a1ca4d1238754092d6dbffd831737c5a175b87a8
mystagogy <hi7747@naver.com>
2023-02-20 13:54:32 +0900

    #38 refactor : DTO 선언 타입, 변수 변경

--- src/main/java/com/miniproject/backend/loanproduct/dto/ProductListDTO.java
+++ src/main/java/com/miniproject/backend/loanproduct/dto/ProductListDTO.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class ProductListDTO {
 
     @Schema(description = "상품아이디", defaultValue = "1")
-    private Long id;
+    private String id;
     @Schema(description = "상품 이름", defaultValue = "test대출")
     private String productName;
     @Schema(description = "은행 이름", defaultValue = "test은행")
@@ -23,25 +23,16 @@ public class ProductListDTO {
     private String bankImg;
     @Schema(description = "카테고리 이름", defaultValue = " ")
     private String categoryName;
-    @Schema(description = "평균금리", defaultValue = "1.11")
-    private Float avgRate;
+    @Schema(description = "금리 정보")
+    private List<LoanRate> loanRateList;
 
     public ProductListDTO(LoanProduct loanProduct) {
-        this.id = loanProduct.getBank().getId();
+        this.id = loanProduct.getId();
         this.productName = loanProduct.getProductNm();
         this.bankName = loanProduct.getBank().getBankNm();
         this.bankImg = loanProduct.getBank().getImgPath();
-        this.avgRate = getAvgRate(loanProduct.getLoanRates());
+        this.loanRateList = loanProduct.getLoanRates();
         this.categoryName = loanProduct.getCategory().getCategoryName();
-    }
-
-    public Float getAvgRate(List<LoanRate> loanRates) {
-        Float avg = 0f;
-        for (LoanRate loanRate : loanRates) {
-            if (loanRate.getAvgRate() != null)
-                avg = loanRate.getAvgRate();
-        }
-        return avg;
     }
 
 }
