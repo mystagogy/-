1de364b00c9b066d77d2a4d38fdde811022ef81e
hamings <wjdgpal9630@gmail.com>
2023-02-20 13:25:08 +0900

    #40 feat: controller 구매리스트 출력, 구매삭제 기능 추가

--- src/main/java/com/miniproject/backend/shoppingbasket/controller/OrderController.java
+++ src/main/java/com/miniproject/backend/shoppingbasket/controller/OrderController.java
@@ -5,9 +5,10 @@ import com.miniproject.backend.shoppingbasket.dto.BasketResponseDTO;
 import com.miniproject.backend.shoppingbasket.service.OrderService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 @RequiredArgsConstructor
 @RestController
 public class OrderController {
@@ -19,5 +20,19 @@ public class OrderController {
     public ResponseDTO<?> buyCart(@RequestParam String email, @RequestParam String productId){
         BasketResponseDTO basketResponseDTO = orderService.buyCart(email, productId);
         return new ResponseDTO<>().ok(basketResponseDTO,"성공적으로 구매 완료되었습니다!");
+    }
+
+    @Operation(description = "구매 리스트 출력하는 api")
+    @GetMapping("/order")
+    public ResponseDTO<?> showBuyList(@RequestParam String email){
+        List<BasketResponseDTO> basketResponseDTO = orderService.selectBuyList(email);
+        return new ResponseDTO<>().ok(basketResponseDTO,"정상출력 데이터");
+    }
+
+    @Operation(description = "구매 삭제하는 api")
+    @DeleteMapping("/order")
+    public ResponseDTO<?> deleteBuy(@RequestParam String email, @RequestParam Long basketId){
+        String msg = orderService.deleteBuy(email, basketId);
+        return new ResponseDTO<>().ok(msg,"성공적으로 삭제되었습니다.");
     }
 }
