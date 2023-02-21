8d617b41fbc3aac59544c0a184446d9dcde130cc
charcoal <devsj3586@gmail.com>
2023-02-21 15:34:22 +0900

    #55 refactor : 변경된 DTO 적용

--- src/main/java/com/miniproject/backend/loanproduct/service/ProductService.java
+++ src/main/java/com/miniproject/backend/loanproduct/service/ProductService.java
@@ -2,21 +2,21 @@ package com.miniproject.backend.loanproduct.service;
 
 import com.miniproject.backend.loanproduct.domain.LoanProduct;
 import com.miniproject.backend.loanproduct.dto.PagingDTO;
-import com.miniproject.backend.loanproduct.dto.ProductDetailDTO;
-import com.miniproject.backend.loanproduct.dto.ProductListDTO;
+import com.miniproject.backend.loanproduct.dto.ProductDto;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 
 import java.util.List;
+
 @Service
 public interface ProductService {
 
-    List<ProductListDTO> selectProductList ();
+    List<ProductDto.Response> selectProductList();
 
     LoanProduct findProductByProductId(String productId);
 
-    ProductDetailDTO findById (String ProductId);
+    ProductDto.DetailResponse findById(String ProductId);
 
     PagingDTO searchList(String keyword, Pageable pageable);
 }
