275fa5ced13c1cbed8516766c6ffb3a2172e924a
hamings <wjdgpal9630@gmail.com>
2023-02-21 16:16:53 +0900

    #58 refactor: token에서 email 받기, DTO변경

--- src/main/java/com/miniproject/backend/shoppingbasket/service/OrderService.java
+++ src/main/java/com/miniproject/backend/shoppingbasket/service/OrderService.java
@@ -1,17 +1,17 @@
 package com.miniproject.backend.shoppingbasket.service;
 
 import com.miniproject.backend.shoppingbasket.domain.Basket;
-import com.miniproject.backend.shoppingbasket.dto.BasketResponseDTO;
+import com.miniproject.backend.shoppingbasket.dto.BasketDTO;
 
 import java.util.List;
 
 public interface OrderService {
 
-    BasketResponseDTO buyCart(String email, String productId);
+    BasketDTO.Response buyCart(BasketDTO.Request basketRequestDto);
 
     Basket findBasketByUserAndLoanProduct(String email, String productId);
 
-    List<BasketResponseDTO> selectBuyList(String email);
+    List<BasketDTO.Response> selectBuyList(String email);
 
     String deleteBuy(String email,Long basketId);
 
