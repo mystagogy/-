56358e2092a74696b1ba9d6fd78e3c1ae1700534
mystagogy <hi7747@naver.com>
2023-02-23 19:36:17 +0900

    #83 feat : 로그아웃 시 토큰 삭제 로직 추가

--- src/main/java/com/miniproject/backend/user/service/TokenService.java
+++ src/main/java/com/miniproject/backend/user/service/TokenService.java
@@ -5,4 +5,5 @@ import com.miniproject.backend.user.domain.User;
 public interface TokenService {
     public void createRefreshToken(User user, String token);
     public User checkValid(String token);
+    public void delete(String token);
 }
