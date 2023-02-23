23c20b38bfd23a86e2f48cd95fa091fc0204bb70
charcoal <devsj3586@gmail.com>
2023-02-23 20:24:11 +0900

    #68 feat : 회원 정보수정 서비스 구현

--- src/main/java/com/miniproject/backend/user/service/UserService.java
+++ src/main/java/com/miniproject/backend/user/service/UserService.java
@@ -2,15 +2,20 @@ package com.miniproject.backend.user.service;
 
 import com.miniproject.backend.user.domain.User;
 import com.miniproject.backend.user.dto.UserRequestDTO;
-
-import java.util.Optional;
+import com.miniproject.backend.user.dto.UserUpdateDTO;
 
 public interface UserService {
     User signup(UserRequestDTO userRequestDTO);
+
     User findUserByUserId(String userEmail);
+
     Boolean updatePassword(String email, String newPassword);
+
     Boolean matchedPasswords(String email, String password);
+
     Boolean checkDuplicationEmail(String email);
 
     Boolean deleteUser(String email);
+
+    Boolean updateUser(String email, UserUpdateDTO.update update);
 }
