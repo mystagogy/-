aa06698d72d500b941af004b3f73784a4d3e5806
HongGuma <hongsu9849@gmail.com>
2023-02-23 21:00:34 +0900

    fix : 사용자 정보 없음 errorCode 수정

--- src/main/java/com/miniproject/backend/user/exception/UserExceptionType.java
+++ src/main/java/com/miniproject/backend/user/exception/UserExceptionType.java
@@ -6,7 +6,7 @@ public enum UserExceptionType implements CustomExceptionType {
     DUPLICATION_EMAIL(-101,"중복된 이메일 입니다."),
     NOT_EMAIL_FORMAT(-102, "이메일 형식이 아닙니다."),
     ACCOUNT_NOT_MATCH(-103, "이메일 또는 비밀번호가 일치하지 않습니다."),
-    ACCOUNT_NOT_EXIST(-401, "사용자정보가 존재하지 않습니다."),
+    ACCOUNT_NOT_EXIST(-104, "사용자정보가 존재하지 않습니다."),
     UNMATCHED_PASSWORD(-105, "비밀번호가 일치하지 않습니다."),
     PARSING_FAIL(-111,"토큰 파싱에 실패했습니다."),
     NOT_EXIST_REFRESH(-112,"Refresh Token이 없거나 유효하지 않습니다. '/login' 으로 재요청 하십시오."),
