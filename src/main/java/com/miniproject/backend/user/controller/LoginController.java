1d85972522790c73718f694fa56d432363a5b392
mystagogy <hi7747@naver.com>
2023-02-23 13:23:57 +0900

    #74 feat : 로그인 시 refresh token 테이블 저장 및 쿠키 저장 기능 추가

--- src/main/java/com/miniproject/backend/user/controller/LoginController.java
+++ src/main/java/com/miniproject/backend/user/controller/LoginController.java
@@ -1,18 +1,22 @@
 package com.miniproject.backend.user.controller;
 
 import com.miniproject.backend.global.dto.ResponseDTO;
-import com.miniproject.backend.global.jwt.CookieProvider;
 import com.miniproject.backend.global.jwt.auth.AuthToken;
 import com.miniproject.backend.global.jwt.auth.AuthTokenProvider;
 import com.miniproject.backend.user.domain.User;
 import com.miniproject.backend.user.dto.LoginRequestDTO;
 import com.miniproject.backend.user.service.LoginService;
+import com.miniproject.backend.user.service.TokenService;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseCookie;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,19 +25,25 @@ public class LoginController {
 
     private final LoginService loginService;
     private final AuthTokenProvider authTokenProvider;
-    private final CookieProvider cookieProvider;
+    private final TokenService tokenService;
+
 
     @PostMapping("/login")
-    public ResponseDTO<?> login(@RequestBody LoginRequestDTO requestDTO){
+    public ResponseDTO<?> login(HttpServletResponse response, @RequestBody LoginRequestDTO requestDTO) {
 
         User user = loginService.login(requestDTO);
         AuthToken authToken = authTokenProvider.issueAccessToken(user);
-        AuthToken refreushToken = authTokenProvider.issueRefreshToken(user);
-        loginService.updateRefresh(user, refreushToken);
-        ResponseCookie tokenCookie = cookieProvider.createRefreshTokenCookie(refreushToken.getToken());
+        AuthToken refreshToken = authTokenProvider.issueRefreshToken(user);
+
+        Cookie cookie = new Cookie("refresh", refreshToken.getToken());
+        tokenService.createRefreshToken(user,refreshToken.getToken());
+
+        response.addCookie(cookie);
+
 
         log.info(String.valueOf(authToken.getToken()));
-
-        return new ResponseDTO<>().ok(authToken.getToken(),"로그인 성공");
+        return new ResponseDTO<>().ok(authToken.getToken(), "로그인 성공");
     }
+
+
 }
